6e3a4b0fff4951f7171ca171b30d220b66da93c2
Saurav-S-Purushothaman <saurav.kudajadri@gmail.com>
2023-08-09 19:44:53 +0530

    added description

--- scrap_amazon/scrap_amazon/spiders/quotes_spider.py
+++ scrap_amazon/scrap_amazon/spiders/quotes_spider.py
@@ -6,6 +6,15 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response, **kwargs):
+        """ Function to parse url, crawl using xpath/css 
+            selector
+
+        Args:
+            response (scrapy.http.response.html.HtmlResponse): Response from url
+
+        Yields:
+            dictionary: output or result
+        """
         all_div_tags = response.css("div.quote")
         for quote in all_div_tags:
             title = quote.css("span.text::text").extract()
